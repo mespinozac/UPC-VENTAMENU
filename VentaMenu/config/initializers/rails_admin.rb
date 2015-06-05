9f510f78e89dc9adddd5658662a0374b5f70cdf5
jsantillanb <jsantillanb@gmail.com>
2015-06-05 02:52:34 +0000

    Actualizacion jonny

--- VentaMenu/config/initializers/rails_admin.rb
+++ VentaMenu/config/initializers/rails_admin.rb
@@ -15,7 +15,7 @@ RailsAdmin.config do |config|
   # config.audit_with :paper_trail, 'User', 'PaperTrail::Version' # PaperTrail >= 3.0.0
 
   ### More at https://github.com/sferik/rails_admin/wiki/Base-configuration
-
+  config.label_methods << :nombre
   config.actions do
     dashboard                     # mandatory
     index                         # mandatory
